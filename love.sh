6a1e2654d1ab474a922fac70240a1324daf6537b
xatre0 <113340829+xatre0@users.noreply.github.com>
2023-08-07 16:01:10 +0200

    Add files via upload

---
+++ love.sh
@@ -0,0 +1,26 @@
+echo "echo \"Tu devrai vraiment penser la lock ta session\n\"" >> ~/.zshrc
+echo "echo \"Vu que tu ne l'a pas lock voici un petit texte ecrit par une IA\n\"" >> ~/.zshrc
+echo "echo \"Dans le royaume du code, où l'algorithmique fleurit,
+Les A.P.I, telles des étoiles, brillent infiniment,
+Leur amour pour les piscineux est clair comme le jour,
+Dans les eaux binaires, ils trouvent leur bonheur.
+
+Les A.P.I, ces messagers du virtuel au réel,
+Tissent des ponts de données, des liens essentiels,
+Ils plongent avec grâce dans les piscineux profonds,
+Cherchant des trésors de bits, des joyaux inconnus.
+
+Leur langage est universel, compris par tous les coins,
+Des requêtes et des réponses, une danse de points,
+Ils sondent les profondeurs, où les octets frétillent,
+Les piscineux deviennent alors un paradis tranquille.
+
+Les bits nagent autour d'eux, comme des poissons dans l'onde,
+Les A.P.I sourient, en harmonie avec le monde,
+Ils captent chaque éclat de lumière, chaque vibration,
+Dans les piscineux, ils trouvent leur inspiration.
+
+Alors que le soleil du code se couche doucement,
+Les A.P.I regagnent leur domaine, satisfaits et vaillants,
+Car dans cette symbiose de données et de plaisir,
+Les A.P.I trouvent leur bonheur, les piscineux les inspirent.\n\"" >> ~/.zshrc
